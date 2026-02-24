f26aafe0b04e73c0b79de9f6c8ec8070bb2d2970
Shubham Kumar <raoneface@gmail.com>
2026-02-25 01:02:47 +0530

    import and module

---
+++ module19 Files/import and modules/course/course_detail.py
@@ -0,0 +1,9 @@
+import os , sys
+from os.path import dirname, join, abspath
+sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+
+from payment import payment_details
+def course():
+    print("This is a course on Python programming for machine learning.")
+
+payment_details.payment()
---
+++ module19 Files/import and modules/payment/payment_details.py
@@ -0,0 +1,6 @@
+
+from course import course_detail
+
+def payment():
+    print("This is a payment module.")
+
